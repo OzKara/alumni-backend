d13eb7db3c26b71f31fcb840bc4e32d98448f4bd
OzKara <ozankarapri@gmail.com>
2023-10-04 11:22:28 +0200

    Initialize project with Spring, added PostgreSQL and JPA dependencies

---
+++ src/main/java/accelerate/alumni/alumnibackend/AlumniBackendApplication.java
@@ -0,0 +1,13 @@
+package accelerate.alumni.alumnibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniBackendApplication.class, args);
+	}
+
+}
