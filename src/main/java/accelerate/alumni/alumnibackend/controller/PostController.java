9f9ba0e0111c26a9d9b98373adab9a5eae2bfa78
HennningS <79580243+HennningS@users.noreply.github.com>
2023-10-21 18:53:37 +0200

    Update PostController.java

--- src/main/java/accelerate/alumni/alumnibackend/controller/PostController.java
+++ src/main/java/accelerate/alumni/alumnibackend/controller/PostController.java
@@ -16,6 +16,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -23,6 +24,7 @@ import java.security.Principal;
 import java.util.Collection;
 import java.util.Optional;
 
+@PreAuthorize("hasRole('user')")
 @RestController
 @RequestMapping("/api/v1/posts")
 public class PostController {
