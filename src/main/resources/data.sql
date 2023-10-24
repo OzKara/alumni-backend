d8bc8b70e1a4377c11a42874b5831a4a920edd7a
HennningS <henningsletner.70@gmail.com>
2023-10-24 21:00:06 +0200

    Updated the data.sql file to have better inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,31 @@
 INSERT INTO Users (id, name, picture, status, bio, fun_fact)
-VALUES ('nicholas',
+VALUES ('4535e26b-692c-4fc6-a1c9-9e164d1daf7c',
         'Nicholas Lennox',
-        'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg',
-        'Smol criminal',
-        'Introducing Eivind, a curious and adventurous cat who enjoys exploring new places and playing with toys that make crinkly sounds.',
-        'Did you know that Oslo is home to the world''s longest art gallery, which is located in the city''s subway system and features over 100 unique works of art?');
+        'https://fullstack-oslo-aug-23.slack.com/team/U05LDCDG932',
+        'Teaching important stuff',
+        'Probably the best Java instructor in the world and i LOVE php, i think its the best programming language in the world <3',
+        'My favorite food is pizza-rolls and pop-tarts and i like to combine them and it it for dinner');
 INSERT INTO Users (id, name, picture, status, bio, fun_fact)
 VALUES ('d7ab172c-7cab-4bf1-b94e-3e38a7400793',
         'Hashir Raja',
         'https://i.ytimg.com/vi/fOd16PT1S7A/maxresdefault.jpg',
-        'The Great Catsby',
-        'Meet Martin, a playful and cuddly feline who loves nothing more than chasing laser pointers and napping in sunny spots around the house.',
-        'Why is it so easy to weigh tuna? - They have their own scales');
+        'Noe dukket opp',
+        'Sleeping is my thing, football is my swing',
+        'Ble kontaktet av et modellfirma som ville at jeg slkulle jobbe for dem, men valgte et huble life i stedet');
 INSERT INTO Users (id, name, picture, status, bio, fun_fact)
 VALUES ('7e68cd4b-7bd0-4ab1-b2df-09bf54f7e87e',
         'Henning Sletner',
-        'https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg',
-        'Cat me some slack, will you?',
-        'A cat above the rest.',
-        'Purr-haps we can cuddle later.');
+        'https://fullstack-oslo-aug-23.slack.com/team/U05M987B2U8',
+        'Working hard or hardly working',
+        'På god vei til å nå mine fremtidige planer om at all iskrem blir ulovlig.',
+        'Er ca 197cm høy, men nei jeg har ikke gjort noe i livet som man skulle tro en høy perosn har gjort.');
 INSERT INTO Users (id, name, picture, status, bio, fun_fact)
-VALUES ('ozan',
+VALUES ('71677904-aa91-4894-96b7-45521a921172',
         'Ozan Kara',
-        'https://i.pinimg.com/564x/32/f2/43/32f24381b05fcf53d8088c98963fe326.jpg',
-        'It''s a cat-astrophy!',
-        'Born fluffy, still fluffy.',
-        'What do you call a tuna with a tie? - Sofishticated');
+        'https://fullstack-oslo-aug-23.slack.com/team/U05M988LNJC',
+        'Trøbbel med dama',
+        'Vet ikke helt hva jeg driver med, men det funker!',
+        'Flytter kanskje til Barcelona snart, får se hvor sur dama er.');
 INSERT INTO Users (id, name, picture, status, bio, fun_fact)
 VALUES ('700a6c07-f116-4cb4-bf7e-0930a9d4b9ad',
         'Lucas Tran',
@@ -35,12 +35,12 @@ VALUES ('700a6c07-f116-4cb4-bf7e-0930a9d4b9ad',
         'FUN FACT ABOUT ME');
 
 INSERT INTO Groups ( name, description, color, is_private)
-VALUES ('GRUPPPE 1',
-        'dette er gruppe 1 og dette er ikke en gøy gruppe!',
+VALUES ('Vi som elsker maling',
+        'Dette er en gruppe for de som er lidenskapelig opptatt av maling og vil dele den gleden med andre!',
         '#FF1493', true);
 INSERT INTO Groups (name, description, color, is_private)
-VALUES ( 'Gruppe 2',
-         'Dette er gruppe 2 og dette er hvertfall ikke en gøy gruppe',
+VALUES ( 'Jorda er illusjon',
+         'Dette er en gruppe for de som mener at jordkloden er en illusjon og ikke eksisterer',
          '#008B8B', true);
 INSERT INTO Groups (name, description, color, is_private)
 VALUES ( 'JAVA KURS',
@@ -55,11 +55,11 @@ INSERT INTO group_user ("groups_id", "users_id")
 VALUES (1, '700a6c07-f116-4cb4-bf7e-0930a9d4b9ad');
 VALUES (2, '7e68cd4b-7bd0-4ab1-b2df-09bf54f7e87e');
 INSERT INTO group_user ("groups_id", "users_id")
-VALUES (2, 'ozan');
+VALUES (2, '71677904-aa91-4894-96b7-45521a921172');
 INSERT INTO group_user ("groups_id", "users_id")
 VALUES (2, 'd7ab172c-7cab-4bf1-b94e-3e38a7400793');
 INSERT INTO group_user ("groups_id", "users_id")
-VALUES (2, 'nicholas');
+VALUES (2, '4535e26b-692c-4fc6-a1c9-9e164d1daf7c');
 INSERT INTO group_user ("groups_id", "users_id")
 VALUES (2, '700a6c07-f116-4cb4-bf7e-0930a9d4b9ad');
 INSERT INTO group_user ("groups_id", "users_id")
@@ -83,7 +83,7 @@ VALUES ('2023-04-02 14:45', '2023-04-02', '700a6c07-f116-4cb4-bf7e-0930a9d4b9ad'
 
 -- Post 3
 INSERT INTO Post (created_at, updated_at, owner_id, origin_id, title, content, group_id)
-VALUES ('2023-04-03 12:30', '2023-04-03', 'ozan', 1,
+VALUES ('2023-04-03 12:30', '2023-04-03', '71677904-aa91-4894-96b7-45521a921172', 1,
         'Simplicity in Design',
         'Designing with simplicity in mind is like telling a story with few words - it ensures clarity and ease of use.' ||
         '![simplicity](https://example.com/simplicity-image.jpg)', 2);
@@ -94,3 +94,9 @@ VALUES ('2023-04-04 17:20', '2023-04-04', 'd7ab172c-7cab-4bf1-b94e-3e38a7400793'
         'Responsive Web Design',
         'Creating responsive web designs is like building a flexible house - it adapts to different screen sizes and devices.' ||
         '![responsive design](https://example.com/responsive-design-image.jpg)', 2);
+
+-- Post 4
+INSERT INTO Post (created_at, updated_at, owner_id, origin_id, title, content, group_id)
+VALUES ('2023-10-10 12:25', '2023-04-04', '71677904-aa91-4894-96b7-45521a921172', 1,
+        'Stor dag i dag',
+        'Stor dag i dag, presentasjon på Noroff. Kommer til å gå veldig not so good. Lol', 2);
